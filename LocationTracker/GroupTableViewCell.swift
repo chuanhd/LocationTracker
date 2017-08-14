612a38d9123d5f723d030cf2746d14de5528c731
chuanhd@iMac <chuan_hd@Chuans-iMac-2.local>
2017-08-14 17:21:10 +0700

    Group list view (wip)

---
+++ LocationTracker/GroupTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupTableViewCell.swift
+//  LocationTracker
+//
+//  Created by Chuan Ho Danh on 8/14/17.
+//  Copyright © 2017 tranght. All rights reserved.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ LocationTracker/ListGroupsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ListGroupsViewController.swift
+//  LocationTracker
+//
+//  Created by Chuan Ho Danh on 8/14/17.
+//  Copyright © 2017 tranght. All rights reserved.
+//
+
+import UIKit
+
+class ListGroupsViewController: UIViewController {
+
+    @IBOutlet weak var tblListGroups: UITableView!
+    @IBOutlet weak var btnJoinGroup: UIButton!
+    @IBOutlet weak var btnCreateGroup: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.btnJoinGroup.clipsToBounds = true
+        self.btnJoinGroup.layer.borderWidth = 2.0
+        self.btnJoinGroup.layer.borderColor = UIColor(colorLiteralRed: 83, green: 181, blue: 169, alpha: 1.0).cgColor
+        self.btnJoinGroup.layer.cornerRadius = 10.0
+        
+        self.btnCreateGroup.layer.borderWidth = 2.0
+        self.btnCreateGroup.layer.borderColor = UIColor.white.cgColor
+        self.btnCreateGroup.layer.cornerRadius = 10.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
